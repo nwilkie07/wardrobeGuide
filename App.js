f085d23e7ced631b3992600ff6fac6ec37fd159d
whjfung <77548862+whjfung@users.noreply.github.com>
2021-09-19 01:31:05 +0800

    nav-bar

--- App.js
+++ App.js
@@ -3,12 +3,85 @@ import jwtDecode from 'jwt-decode';
 import * as React from 'react';
 import { Alert, Button, Platform, StyleSheet, Text, View, Pressable } from 'react-native';
 import { REACT_APP_AUTH_CLIENT_ID, REACT_APP_AUTH_ENDPOINT } from '@env';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const auth0ClientId = REACT_APP_AUTH_CLIENT_ID;
 const authorizationEndpoint = REACT_APP_AUTH_ENDPOINT;
 
 const useProxy = Platform.select({ web: false, default: true });
 const redirectUri = AuthSession.makeRedirectUri({ useProxy });
+
+const Tab = createMaterialBottomTabNavigator();
+
+function Camera() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Feed!</Text>
+    </View>
+  );
+}
+
+function Wardrobe() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile!</Text>
+    </View>
+  );
+}
+
+function Outfits() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notifications!</Text>
+    </View>
+  );
+}
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Feed"
+      activeColor="white"
+      inactiveColor="#D0C8CE"
+      labelStyle={{ fontSize: 12 }}
+      barStyle={{ backgroundColor: '#A228FF' }}
+    >
+      <Tab.Screen
+        name="Camera"
+        component={Camera}
+        options={{
+          tabBarLabel: 'Camera',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="camera" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Outfits"
+        component={Outfits}
+        options={{
+          tabBarLabel: 'Outfits',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="tshirt-crew" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Wardrobe"
+        component={Wardrobe}
+        options={{
+          tabBarLabel: 'Wardrobe',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="wardrobe" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
 
 export default function App() {
   const [name, setName] = React.useState(null);
@@ -57,20 +130,19 @@ export default function App() {
   }, [result]);
 
   return (
-    <View style={styles.container}>
+    <>
       {name ? (
-        <>
-          <Text style={styles.title}>Hello {name}!</Text>
-          <Pressable style={styles.button} onPress={() => setName(null)} >
-            <Text style={styles.text}>Log Out</Text>
-          </Pressable>
-        </>
+        <NavigationContainer>
+          <MyTabs />
+        </NavigationContainer>
       ) : (
+        <View style={styles.container}>
         <Pressable style={styles.button} disabled={!request} onPress={() => promptAsync({ useProxy })}>
           <Text style={styles.text}>Log In</Text>
-        </Pressable>
+          </Pressable>
+        </View>
       )}
-    </View>
+    </>
   );
 }
 
@@ -88,7 +160,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 32,
     borderRadius: 4,
     elevation: 3,
-    backgroundColor: 'black',
+    backgroundColor: '#D0C8CE',
   },
   text: {
     fontSize: 16,
