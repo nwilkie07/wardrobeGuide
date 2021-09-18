d43f84cc62dc296a58e892797f9720904b7b19ec
Nick <57763842+nwilkie07@users.noreply.github.com>
2021-09-18 12:46:59 -0700

    Functioning Camera
    Camera tab asks for camera permissions and opens camera. A button can flip the camera view and a button takes pictures and stores them in the apps temp directory.

--- App.js
+++ App.js
@@ -1,31 +1,91 @@
-import * as AuthSession from 'expo-auth-session';
-import jwtDecode from 'jwt-decode';
-import * as React from 'react';
-import { Alert, Button, Platform, StyleSheet, Text, View, Pressable } from 'react-native';
-import { REACT_APP_AUTH_CLIENT_ID, REACT_APP_AUTH_ENDPOINT } from '@env';
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import * as AuthSession from "expo-auth-session";
+import jwtDecode from "jwt-decode";
+import React, { useState, useEffect } from "react";
+import {
+  Alert,
+  Button,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
+import { REACT_APP_AUTH_CLIENT_ID, REACT_APP_AUTH_ENDPOINT } from "@env";
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Camera } from "expo-camera";
+import { useIsFocused } from "@react-navigation/native";
 
-const auth0ClientId = REACT_APP_AUTH_CLIENT_ID;
-const authorizationEndpoint = REACT_APP_AUTH_ENDPOINT;
+const auth0ClientId = "lwLIMn3PErALFA3LM6aculLTIuZwqIxd";
+const authorizationEndpoint = "https://dev-zvi726ys.us.auth0.com/authorize";
 
 const useProxy = Platform.select({ web: false, default: true });
 const redirectUri = AuthSession.makeRedirectUri({ useProxy });
 
 const Tab = createMaterialBottomTabNavigator();
 
-function Camera() {
+function CameraTab() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Feed!</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      {isFocused && (
+        <Camera
+          style={{ flex: 1 }}
+          type={type}
+          ref={(ref) => {
+            this.camera = ref;
+          }}
+        >
+          <View>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                setType(
+                  type === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back
+                );
+              }}
+            >
+              <Text style={styles.text}> Flip </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                this.camera.takePictureAsync();
+              }}
+            >
+              <Text style={styles.text}> Capture </Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      )}
+    </SafeAreaView>
   );
 }
 
 function Wardrobe() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Profile!</Text>
     </View>
   );
@@ -33,7 +93,7 @@ function Wardrobe() {
 
 function Outfits() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Notifications!</Text>
     </View>
   );
@@ -46,13 +106,13 @@ function MyTabs() {
       activeColor="white"
       inactiveColor="#D0C8CE"
       labelStyle={{ fontSize: 12 }}
-      barStyle={{ backgroundColor: '#A228FF' }}
+      barStyle={{ backgroundColor: "#A228FF" }}
     >
       <Tab.Screen
-        name="Camera"
-        component={Camera}
+        name="CameraTab"
+        component={CameraTab}
         options={{
-          tabBarLabel: 'Camera',
+          tabBarLabel: "CameraTab",
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="camera" color={color} size={26} />
           ),
@@ -62,9 +122,13 @@ function MyTabs() {
         name="Outfits"
         component={Outfits}
         options={{
-          tabBarLabel: 'Outfits',
+          tabBarLabel: "Outfits",
           tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="tshirt-crew" color={color} size={26} />
+            <MaterialCommunityIcons
+              name="tshirt-crew"
+              color={color}
+              size={26}
+            />
           ),
         }}
       />
@@ -72,7 +136,7 @@ function MyTabs() {
         name="Wardrobe"
         component={Wardrobe}
         options={{
-          tabBarLabel: 'Wardrobe',
+          tabBarLabel: "Wardrobe",
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="wardrobe" color={color} size={26} />
           ),
@@ -82,7 +146,6 @@ function MyTabs() {
   );
 }
 
-
 export default function App() {
   const [name, setName] = React.useState(null);
 
@@ -91,12 +154,12 @@ export default function App() {
       redirectUri,
       clientId: auth0ClientId,
       // id_token will return a JWT token
-      responseType: 'id_token',
+      responseType: "id_token",
       // retrieve the user's profile
-      scopes: ['openid', 'profile'],
+      scopes: ["openid", "profile"],
       extraParams: {
         // ideally, this will be a random value
-        nonce: 'nonce',
+        nonce: "nonce",
       },
     },
     { authorizationEndpoint }
@@ -110,16 +173,16 @@ export default function App() {
     if (result) {
       if (result.error) {
         Alert.alert(
-          'Authentication error',
-          result.params.error_description || 'something went wrong'
+          "Authentication error",
+          result.params.error_description || "something went wrong"
         );
         return;
       }
-      if (result.type === 'success') {
+      if (result.type === "success") {
         // Retrieve the JWT token and decode it
         const jwtToken = result.params.id_token;
         const decoded = jwtDecode(jwtToken);
-        
+
         console.log(decoded);
         const { name } = decoded;
         const { nickname } = decoded;
@@ -137,8 +200,12 @@ export default function App() {
         </NavigationContainer>
       ) : (
         <View style={styles.container}>
-        <Pressable style={styles.button} disabled={!request} onPress={() => promptAsync({ useProxy })}>
-          <Text style={styles.text}>Log In</Text>
+          <Pressable
+            style={styles.button}
+            disabled={!request}
+            onPress={() => promptAsync({ useProxy })}
+          >
+            <Text style={styles.text}>Log In</Text>
           </Pressable>
         </View>
       )}
@@ -149,24 +216,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   button: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingVertical: 12,
     paddingHorizontal: 32,
     borderRadius: 4,
     elevation: 3,
-    backgroundColor: '#D0C8CE',
+    backgroundColor: "#D0C8CE",
   },
   text: {
     fontSize: 16,
     lineHeight: 21,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     letterSpacing: 0.25,
-    color: 'white',
+    color: "white",
   },
 });
